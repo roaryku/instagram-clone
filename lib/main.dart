e7927f25e88eb42f13b139bf49dfebf241d76ccd
Rodika Aryku <raryku@yahoo.com>
2024-10-23 10:20:14 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(OurInstagramClone);
+}
+
+// widget - buttons, p, headers, list, links, image
+
+
+class OurInstagramClone extends StatelessWidget {
+  const OurInstagramClone({super.key})
+}
