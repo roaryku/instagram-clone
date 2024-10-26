9936c39896a612316b8869f3ba38fcf3f6f0190e
Rodika Aryku <raryku@yahoo.com>
2024-10-25 17:47:25 -0700

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(OurInstagramClone);
+  runApp(const OurInstagramClone());
 }
-
-// widget - buttons, p, headers, list, links, image
-
 
 class OurInstagramClone extends StatelessWidget {
-  const OurInstagramClone({super.key})
+  const OurInstagramClone ({super.key});
 }
+
+@override
+Widget build(BuildContext context) {
+  return const MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void _print() {
+    print('IT WAS CLICKED!');
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      ElevatedButton(onPressed: _print, child: Text('CLICK HERE')),
+      Text('HEY')
+    ],);
+  }
+}
+
+
